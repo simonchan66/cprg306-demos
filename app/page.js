9f60f3bc43685d6d5e4340e195090558f3fdb05a
simonchan66 <formul529@gmail.com>
2024-01-16 13:12:27 -0700

    add new page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Link href="/week-2">Press on me to link to page2</Link>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-Hello World
+Hello World testing testing
       </div>
+      <p></p>
     </main>
   )
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,6 +1,9 @@
+import Heading from './heading';
+
 export default function Page() {
     return (
 <main className="text-4xl">
+<Heading />
 Hello this is another page
 </main>
 
